24b1d0bebbb7443884158692605a6ca549a0ec00
Shubham <theshubham.dev@gmail.com>
2023-11-04 23:43:16 +0530

    complete

--- swiftDiceApp/ViewController.swift
+++ swiftDiceApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
-
+    @IBAction func rollButton(_ sender: UIButton) {
+       
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+              diceImageViewLeft.image = diceArray[Int.random(in: 0...5)]
+              diceImageViewRight.image = diceArray[Int.random(in: 0...5)]
+              
+    }
 }
 
